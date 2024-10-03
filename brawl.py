b8ad990c563cff2abf5b1c9fd7c7c723987b81ef
Addeballe <adrian.nilsson@elev.ga.ntig.se>
2024-10-03 09:40:46 +0200

    addat vapen och sånt

--- brawl.py
+++ brawl.py
@@ -1,64 +1,79 @@
 import random
 
-#Namn och välkommanden
-print ("Välkommen till mitt Den Stora Duellen! ")
+#-----------------------------------------------Game Variables---------------------------------
+spelare_ett_liv = 100
+spelare_två_liv = 100
 
-spelare_ett_namn = input("Skriv in namnet för spelare ett:")
-spelare_två_namn = input("Skriv in namnet för spelare två:")
-#spelare_ett_klass = input("Skirv in klass för spelare ett ():")
-
-#spelvariabler
-spelare_ett_liv = 10
-spelare_två_liv = 10
-rundantal = 1
 combat_hit = ['magen', 'armen', 'huvudet', 'benet']
+
 player_one_trött = True
 player_two_trött = True
 player_one_sista = True
-Player_two_sista = True
-#spelvariabler
+player_two_sista = True
 
-print (f"{spelare_ett_namn} och {spelare_två_namn} står mot varandra i arenan. Den med som först tar ned sin motståndare vinner! Må den bästa krigaren vinna!")
+vapen = ['spikklubba', 'spjut', 'långsvärd', 'kortsvärd', 'dolk', 'inget vapen/händer']
+skydd = ['ringbrynja', 'plåtrustning', 'läder', 'naken']
+sekundär = ['sköld', 'parrydolk', 'mini-armborst']
+
+player_one_attack = 0
+player_one_defence = 0
+player_one_movement = 0
+
+player_two_attack = 0
+player_two_defence = 0
+player_two_movement = 0
+#-------------------------------------------------------------------------------------------------
+
+print ("Välkommen till mitt Den Stora Duellen! ")
+spelare_ett_namn = input("Skriv in namnet för krigare ett:")
+spelare_två_namn = input("Skriv in namnet för krigare två:")
+print (f"{spelare_ett_namn} och {spelare_två_namn} möter varandra i arenan. Den med som först tar ned sin motståndare vinner! Må den bästa krigaren vinna!")
 spel_aktivitet = input("Starta Duellen?(Y/N)")
+
+
 
 if spel_aktivitet == "N":
     print("Men varför är du ens här då? Fegis..")
 
-#combat
-while True:
-    spelare_ett_attack = random.randint(1,20)
-    spelare_två_attack = random.randint(1,20)
+
+
+print("Den lokala smeden går ut på arenan. ")
+
+
+
+while True: 
+    if spel_aktivitet == "N":
+        break
+
+    spelare_ett_combatroll = random.randint(1,20)
+    spelare_två_combatroll = random.randint(1,20)
     
-    
-    
-    if spelare_ett_attack >= spelare_två_attack:
+    if spelare_ett_combatroll > spelare_två_attack:
         spelare_två_liv -= 1
         print(f"{spelare_ett_namn} lyckas slå till {spelare_två_namn} i {random.choice(combat_hit)}!")
-    elif spelare_två_attack >= spelare_ett_attack:
+    elif spelare_två_attack > spelare_ett_attack:
         spelare_ett_liv -= 1
         print(f"{spelare_två_namn} lyckas slå till {spelare_ett_namn} i {random.choice(combat_hit)}!")
     else:
         print("Ingen lyckas slå till varandra!")
 
 
-#fixa detta
-        if spelare_ett_liv == 5 and player_one_trött is True:
-            print(f"{spelare_ett_namn} börjar att bli trött!")
-            player_one_trött = False
+    if spelare_ett_liv <= 50 and player_one_trött is True:
+        print(f"{spelare_ett_namn} börjar att bli trött!")
+        player_one_trött = False
 
-        elif spelare_två_liv == 5 and player_two_trött is True:
-            print(f"{spelare_två_namn} börjar att bli trött!")
-            player_two_trött = False
+    elif spelare_två_liv <= 50 and player_two_trött is True:
+        print(f"{spelare_två_namn} börjar att bli trött!")
+        player_two_trött = False
 
-        elif spelare_ett_liv == 2 and player_one_sista is True:
-            print(f"{spelare_ett_namn} står på sina sista ben!")
-            player_one_sista = False
+    elif spelare_ett_liv <= 20 and player_one_sista is True:
+        print(f"{spelare_ett_namn} står på sina sista ben!")
+        player_one_sista = False
 
-        elif spelare_två_liv == 2 and player_two_sista is True:
-            print(f"{spelare_två_namn} står på sina sista ben!")
-            Player_two_sista = False
-    
-    
-    
+    elif spelare_två_liv <= 20 and player_two_sista is True:
+        print(f"{spelare_två_namn} står på sina sista ben!")
+        Player_two_sista = False
+
     if spelare_ett_liv == 0 or spelare_två_liv == 0:
-       break
+       if spelare_ett_liv 
+       
